50c5024b4d07b3c6559fa2441a082402eb114749
OctavioBelloG <bellooctavio65@gmail.com>
2026-04-17 21:11:10 -0600

    inicio

---
+++ android/app/src/main/kotlin/com/example/punto_de_venta_movil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.punto_de_venta_movil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlataform
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
